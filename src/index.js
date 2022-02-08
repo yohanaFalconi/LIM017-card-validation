8a8ef2d3998b4fc0a85e7326e796914c9a5f5c36
yohanaFalconi <yohanafalconivivas@gmail.com>
2022-02-07 20:33:49 -0500

    cambios_07/02

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 import validator from './validator.js';
-
 console.log(validator);
+
+//solo permite recibir números
+inputCardNumber.addEventListener('keypress', function(event){
+  return (event.charCode >= 48 && event.charCode <= 57);
+})
+
+let message = document.getElementById("message");
+let button1 = document.getElementById("buttonValidation");
+button1.addEventListener("click", playButton);
+
+function playButton(){
+  //capturar el input y almacenarlo
+  var inputCardNumber = document.getElementById("inputCardNumber");
+  inputCardNumber = inputCardNumber.value; 
+  console.log(inputCardNumber);
+
+
+
+
+}
+  /*
+  // envíen un msm de número incompleto ¿cuándo solo ingresa <9?
+  // envía un msm si ingresa e, signo o un campo vacío al final
+  if( isNa0N(cardNumber) ){
+    document.getElementById("message").innerHTML=`Número incompleto, debes insertar 9 caracteres`;
+  } else if(cardNumber==''){
+    // si no ingresa ningún carácter   
+    message.innerHTML=`Campo vacío`;
+  } else{
+    message.innerHTML=`Número completo`;
+  } */
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,5 @@
 const validator = {
-  // ...
-};
-
-export default validator;
+    // ...
+  };
+  
+  export default validator;
